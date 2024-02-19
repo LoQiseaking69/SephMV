aa1bdd77611d857410f0200621f735f80e029321
LoQiseaking69 <130005196+LoQiseaking69@users.noreply.github.com>
2024-02-18 20:05:02 -0500

    Update model.py

--- model.py
+++ model.py
@@ -2,38 +2,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models, optimizers
 import gym
+from collections import deque
 
 # ReplayBuffer implementation
 class ReplayBuffer:
-    def __init__(self, max_size, input_shape, n_actions):
-        self.mem_size = max_size
-        self.mem_cntr = 0
-        self.state_memory = np.zeros((self.mem_size,) + input_shape, dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size,) + input_shape, dtype=np.float32)
-        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=np.int8)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.int32)
+    def __init__(self, max_size):
+        self.buffer = deque(maxlen=max_size)
 
-    def store_transition(self, state, action, reward, new_state, done):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = new_state
-        self.action_memory[index] = action
-        self.reward_memory[index] = reward
-        self.terminal_memory[index] = done
-        self.mem_cntr += 1
+    def store_transition(self, transition):
+        self.buffer.append(transition)
 
     def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_cntr, self.mem_size)
-        batch = np.random.choice(max_mem, batch_size, replace=False)
-
-        states = self.state_memory[batch]
-        actions = self.action_memory[batch]
-        rewards = self.reward_memory[batch]
-        new_states = self.new_state_memory[batch]
-        dones = self.terminal_memory[batch]
-
-        return states, actions, rewards, new_states, dones
+        batch = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, next_states, dones = zip(*[self.buffer[idx] for idx in batch])
+        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(dones)
 
 # RBM Layer definition
 class RBMLayer(layers.Layer):
@@ -43,7 +25,7 @@ class RBMLayer(layers.Layer):
 
     def build(self, input_shape):
         self.rbm_weights = self.add_weight(shape=(input_shape[-1], self.num_hidden_units),
-                                           initializer='random_normal',
+                                           initializer='glorot_uniform',
                                            trainable=True)
         self.biases = self.add_weight(shape=(self.num_hidden_units,),
                                       initializer='zeros',
@@ -55,56 +37,53 @@ class RBMLayer(layers.Layer):
 
 # QLearningLayer definition
 class QLearningLayer(layers.Layer):
-    def __init__(self, action_space_size, state_size, learning_rate=0.01, gamma=0.95, epsilon=0.1):
+    def __init__(self, action_space_size, learning_rate=0.001, gamma=0.99, epsilon=0.1):
         super(QLearningLayer, self).__init__()
         self.action_space_size = action_space_size
-        self.state_size = state_size
         self.learning_rate = learning_rate
         self.gamma = gamma
         self.epsilon = epsilon
-        self.replay_buffer = ReplayBuffer(100000, state_size, action_space_size)
+        self.replay_buffer = ReplayBuffer(100000)
 
     def build(self, input_shape):
-        self.q_network = layers.Dense(self.action_space_size, activation=None)
-        self.optimizer = optimizers.Adam(learning_rate=self.learning_rate)
+        self.q_network = models.Sequential([
+            layers.Dense(256, activation='relu', kernel_initializer='he_uniform'),
+            layers.Dense(self.action_space_size, kernel_initializer='glorot_uniform')
+        ])
+        self.q_network.compile(optimizer=optimizers.Adam(learning_rate=self.learning_rate), loss='mse')
 
     def call(self, state):
         return self.q_network(state)
 
     def update(self, batch_size):
-        if self.replay_buffer.mem_cntr < batch_size:
+        if len(self.replay_buffer.buffer) < batch_size:
             return
 
-        state, action, reward, new_state, done = self.replay_buffer.sample_buffer(batch_size)
+        states, actions, rewards, next_states, dones = self.replay_buffer.sample_buffer(batch_size)
+        q_values = self.q_network.predict(states)
+        next_q_values = self.q_network.predict(next_states)
 
-        with tf.GradientTape() as tape:
-            q_values = self.q_network(state)
-            q_action = tf.reduce_sum(tf.multiply(q_values, tf.one_hot(action, self.action_space_size)), axis=1)
-            next_q_values = self.q_network(new_state)
-            max_next_q_values = tf.reduce_max(next_q_values, axis=1)
-            target_q_values = reward + self.gamma * max_next_q_values * (1 - done)
-            loss = tf.reduce_mean(tf.square(target_q_values - q_action))
+        target_q_values = np.copy(q_values)
+        batch_indices = np.arange(batch_size, dtype=np.int32)
 
-        grads = tape.gradient(loss, self.q_network.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.q_network.trainable_variables))
+        target_q_values[batch_indices, actions] = rewards + (1 - dones) * self.gamma * np.max(next_q_values, axis=1)
+
+        self.q_network.fit(states, target_q_values, epochs=1, verbose=0)
 
     def store_transition(self, state, action, reward, next_state, done):
-        self.replay_buffer.store_transition(state, action, reward, next_state, done)
+        self.replay_buffer.store_transition((state, action, reward, next_state, done))
 
     def choose_action(self, state):
-        if np.random.random() < self.epsilon:
-            action = np.random.choice(self.action_space_size)
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.action_space_size)
         else:
-            state = np.array([state])
-            q_values = self.q_network(state)
-            action = np.argmax(q_values)
-
-        return action
+            q_values = self.q_network.predict(state)
+            return np.argmax(q_values)
 
 # Positional Encoding for Transformer
 def positional_encoding(seq_length, d_model):
     position = tf.range(seq_length, dtype=tf.float32)[:, tf.newaxis]
-    div_term = tf.exp(tf.range(0, d_model, 2, dtype=tf.float32) * -(tf.math.log(10000.0) / d_model))
+    div_term = tf.exp(tf.range(0, d_model, 2, dtype=tf.float32) * -(np.log(10000.0) / d_model))
     sine_terms = tf.sin(position * div_term)
     cosine_terms = tf.cos(position * div_term)
     pos_encoding = tf.concat([sine_terms, cosine_terms], axis=-1)
@@ -117,41 +96,39 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     x = layers.Dropout(dropout)(x)
     res = x + inputs
     x = layers.LayerNormalization(epsilon=1e-6)(res)
-    x = layers.Dense(ff_dim, activation="relu")(x)
+    x = layers.Dense(ff_dim, activation="relu", kernel_initializer='he_uniform')(x)
     x = layers.Dropout(dropout)(x)
-    x = layers.Dense(inputs.shape[-1])(x)
+    x = layers.Dense(inputs.shape[-1], kernel_initializer='glorot_uniform')(x)
     return x + res
 
-# Model building function with integrated LSTM, Conv1D, Transformer, RBM, and Q-Learning layers
+# Model building function with integrated RBM, Q-Learning, LSTM, Conv1D, and Transformer layers
 def create_neural_network_model(seq_length, d_model, num_hidden_units, action_space_size):
     input_layer = layers.Input(shape=(seq_length, d_model))
-    x_lstm = layers.Bidirectional(layers.LSTM(128, return_sequences=True))(input_layer)
-    x_conv = layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu')(x_lstm)
+    x_lstm = layers.Bidirectional(layers.LSTM(128, return_sequences=True, kernel_initializer='glorot_uniform'))(input_layer)
+    x_conv = layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_uniform')(x_lstm)
     x_pos_encoding = positional_encoding(seq_length, d_model)
     x_pos_encoding = tf.expand_dims(x_pos_encoding, axis=0)  # Add batch dimension
     x_pos_encoded = x_conv + x_pos_encoding
     transformer_output = transformer_encoder(x_pos_encoded, head_size=32, num_heads=2, ff_dim=64)
     x_rbm = RBMLayer(num_hidden_units)(transformer_output)
-    q_learning_layer = QLearningLayer(action_space_size, (seq_length, d_model))(x_rbm)
+    q_learning_layer = QLearningLayer(action_space_size)(x_rbm)
     model = models.Model(inputs=input_layer, outputs=q_learning_layer)
     return model
 
 # Training the model in the BipedalWalker environment
-def train_model_in_bipedalwalker(env_name, model, num_episodes):
+def train_model_in_bipedalwalker(env_name, model, num_episodes, batch_size=64):
     env = gym.make(env_name)
     for episode in range(num_episodes):
         initial_state = env.reset()
-        state = np.array(initial_state) if isinstance(initial_state, tuple) else initial_state
-        state = np.expand_dims(state, axis=0)
+        state = np.expand_dims(initial_state, axis=0)
         done = False
         total_reward = 0
         while not done:
             action = model.choose_action(state)
             next_state, reward, done, _ = env.step(action)
-            next_state = np.array(next_state) if isinstance(next_state, tuple) else next_state
             next_state = np.expand_dims(next_state, axis=0)
             model.store_transition(state, action, reward, next_state, done)
-            model.update(64)
+            model.update(batch_size)
             state = next_state
             total_reward += reward
         print(f'Episode {episode + 1}/{num_episodes}, Total Reward: {total_reward}')
@@ -160,10 +137,10 @@ def train_model_in_bipedalwalker(env_name, model, num_episodes):
 # Parameters for the model and training
 env_name = 'BipedalWalker-v3'
 seq_length = 128
-d_model = 32  # Adjusted to match the dimension of positional encoding
-num_hidden_units = 256
+d_model = 32
+num_hidden_units = 64
 action_space_size = 4
-num_episodes = 100
+num_episodes = 1000
 
 # Create and compile the model
 model = create_neural_network_model(seq_length, d_model, num_hidden_units, action_space_size)
